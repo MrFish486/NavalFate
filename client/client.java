e125da9925e4c6f9d1bdb74de83c1b838cb101f0
MrFish486 <laszlodeanpav@gmail.com>
2025-07-21 12:43:04 -0700

    Finished client

--- client/client.java
+++ client/client.java
@@ -13,15 +13,22 @@ public class client {
 	public static void main (String args[]) throws IOException {
 		try {
 			String        out, in;
-			String        clientversion = "0.1";
+			String        version = "0.1";
 			int           port = 0;
 			byte[]        rcv = new byte[2048];
 			InetAddress address = InetAddress.getByName("0.0.0.0");;
 			Pattern a = Pattern.compile("ship new [a-zA-Z0-9]{1,} [0-9]{1,} [0-9]{1,}");
 			Pattern b = Pattern.compile("ship [a-zA-Z0-9]{1,} move [0-9]{1,} [0-9]{1,}( --speed=[0-9]{1,})?");
-
+ 			Pattern c = Pattern.compile("ship [a-zA-Z0-9]{1,} shoot [0-9]{1,} [0-9]{1,}");
+			Pattern d = Pattern.compile("mine set [0-9]{1,} [0-9]{1,} --moored");
+			Pattern e = Pattern.compile("mine set [0-9]{1,} [0-9]{1,} --drifting");
+			Pattern f = Pattern.compile("mine remove [0-9]{1,} [0-9]{1,}");
+			Pattern g = Pattern.compile("(--version|-v)");
+			Pattern h = Pattern.compile("info [0-9]{1,} [0-9]{1,}");
+			Pattern i = Pattern.compile("(-s|status)");
+			Pattern j = Pattern.compile("(-t|tick)");
 			try {
-				File handle = new File(".navalfate");
+				File handle = new File(System.getProperty("user.home") + "/.navalfate");
 				Scanner read  = new Scanner(handle);
 				String line;
 				while (read.hasNextLine()) {
@@ -46,7 +53,7 @@ public class client {
 				}
 				read.close();
 			} catch (FileNotFoundException ee) {
-				PrintWriter handle = new PrintWriter(".navalfate", "UTF-8");
+				PrintWriter handle = new PrintWriter(System.getProperty("user.home") + "/.navalfate", "UTF-8");
 				BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 				System.out.println("The configuration file is missing. This is likely because this program is being run for the first time.");
 				System.out.println("Please input the ip address of the server.");
@@ -64,8 +71,24 @@ public class client {
 				out = String.format("placeship-%s-%s-%s", args[3], args[4], args[2]);
 			} else if (b.matcher(joinedargs).matches()) {
 				out = String.format("moveship-%s-%s-%s", args[1], args[3], args[4]);
+			} else if (c.matcher(joinedargs).matches()) {
+				out = String.format("shoot-%s-%s-%s", args[1], args[3], args[4]);
+			} else if (d.matcher(joinedargs).matches()) {
+				out = String.format("placemine-%s-%s-moored", args[2], args[3]);
+			} else if (e.matcher(joinedargs).matches()) {
+				out = String.format("placemine-%s-%s-drifting", args[2], args[3]);
+			} else if (f.matcher(joinedargs).matches()) {
+				out = String.format("removemine-%s-%s", args[2], args[3]);
+			} else if (g.matcher(joinedargs).matches()) {
+				out = "version?";
+			} else if (h.matcher(joinedargs).matches()) {
+				out = String.format("info-%s-%s", args[1], args[2]);
+			} else if (i.matcher(joinedargs).matches()) {
+				out = "status?";
+			} else if (j.matcher(joinedargs).matches()) {
+				out = "tick";
 			} else {
-				System.out.println("Naval Fate.\nUsage:\n\tnaval_fate ship new <name> <x> <y>\n\tnaval_fate ship <name> move <x> <y> [--speed=<kn>]\n\tnaval_fate ship shoot <x> <y>\n\tnaval_fate mine (set | remove) <x> <y> [--moored | --drifting]\n\tnaval_fate (-h | --help)\n\tnaval_fate --version");
+				System.out.println("Naval Fate.\nUsage:\n\tnaval_fate ship new <name> <x> <y>\n\tnaval_fate ship <name> move <x> <y> [--speed=<kn>]\n\tnaval_fate ship <name> shoot <x> <y>\n\tnaval_fate mine (set | remove) <x> <y> [--moored | --drifting]\n\tnaval_fate info <x> <y>\n\tnaval_fate (-s | status)\n\tnaval_fate (-t | tick)\n\tnaval_fate (-h | --help)\n\tnaval_fate (--version | -v)");
 				return;
 			}
 			//
@@ -76,7 +99,61 @@ public class client {
 			socket.receive(rcvpacket);
 			in = new String(rcvpacket.getData(), 0, rcvpacket.getLength());
 			// Act based on what the server returned
-			System.out.println(in);
+			if (in.equals("!0") || in.equals("!1") || in.equals("!2")) {
+				System.out.println("The server encountered an error. Consider checking that your client is up-to-date.");
+				return;
+			}
+			if (a.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("Ship was successfully placed.");
+				} else {
+					System.out.println("Failed to place ship.");
+				}
+			} else if (b.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("Ship was successfully moved.");
+				} else {
+					System.out.println("Failed to place move.");
+				}
+			} else if (c.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("The ship hit either another ship or a mine.");
+				} else {
+					System.out.println("The ship missed or doesn't exist.");
+				}
+			} else if (d.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("Mine was successfully placed.");
+				} else {
+					System.out.println("Failed to place mine.");
+				}
+			} else if (e.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("Mine was successfully placed.");
+				} else {
+					System.out.println("Failed to place mine.");
+				}
+			} else if (f.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("Mine was successfully removed.");
+				} else {
+					System.out.println("Failed to place removed.");
+				}
+			} else if (g.matcher(joinedargs).matches()) {
+				System.out.println(String.format("Client version : %s, server version : %s", version, in));
+			} else if (h.matcher(joinedargs).matches()) {
+				System.out.println(in);
+			} else if (i.matcher(joinedargs).matches()) {
+				System.out.println(in);
+			} else if (j.matcher(joinedargs).matches()) {
+				if (in.equals("1")) {
+					System.out.println("Successfully ticked.");
+				} else {
+					System.out.println("Failed to tick.");
+				}
+			} else {
+				System.out.println(String.format("Unknown response from server : '%s'", in));
+			}
 		} catch (IOException ee) {
 			ee.printStackTrace();
 		}
